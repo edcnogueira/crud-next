ddb2ee21444ec6f71bf72a0e5c6c0f420241ce52
edcnogueira <edjan.nogueira14@gmail.com>
2022-03-03 22:47:24 -0300

    feat: cria o client

---
+++ src/core/Client.ts
@@ -0,0 +1,21 @@
+export default class Cliente {
+  #id: string
+  #nome: string
+  #idade: number
+
+  constructor(nome: string, idade: number, id: string) {
+    this.#nome = nome
+    this.#idade = idade
+    this.#id = id
+  }
+
+  get id() {
+    return this.#id
+  }
+  get nome() {
+    return this.#nome
+  }
+  get idade() {
+    return this.#idade
+  }
+}
